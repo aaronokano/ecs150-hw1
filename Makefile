469d9b7e24890386406939691cb354ffd915ac4e
Jason <jason@Jester.(none)>
2012-10-17 12:44:48 -0700

    Finished p3

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
-all: compare connect
+all: compare connect kill_it
 
 compare: p1.c
 	gcc -g p1.c -o compare
 connect: p2.c
 	gcc -g p2.c -o connect
+kill_it: p3.c
+	gcc -g p3.c -o kill_it
---
+++ p3.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+pid_t cpid;
+
+void alarm_handler( int parameter ) {
+  kill( cpid, SIGKILL );
+  exit( 0 );
+}
+
+void print_usage( int code ) {
+  printf("Usage: kill_it <program to run> <time to run program>\n");
+  exit( code );
+}
+
+
+int main( int argc, char* argv[] ) {
+  if( argc != 3 )
+    print_usage( 1 );
+
+  signal( SIGALRM, alarm_handler );
+
+  alarm( atoi( argv[2] ) );
+  cpid = fork();
+  if( cpid == -1 ) {
+    printf("Failed to create child\n");
+    exit( 1 );
+  }
+  else if( cpid == 0 ) {
+    execl( argv[1], NULL );
+  }
+  else {
+    pause();
+  }
+  return 0;
+}
+  
