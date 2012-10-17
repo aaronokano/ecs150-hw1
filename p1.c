0d15bf65b05c9284b5a7d1f2caf1e2c7f22471c5
Aaron Okano <adokano@ucdavis.edu>
2012-10-17 13:29:17 -0700

    Changed a little in p1-p3

--- p1.c
+++ p1.c
@@ -14,21 +14,6 @@ void print_usage( ) {
   exit( 0 );
 }
 
-bool expression( bool s, bool d, int ret1, int ret2 ) {
-  if( d ) {
-    if( s ) {
-      if( ret1 > 0 && ret2 > 0 )
-        return 1;
-      else
-        return 0;
-    }
-  }
-  if( ret1 > 0 || ret2 > 0 )
-    return 1;
-  else
-    return 0;
-}
-
 int min( int one, int two ) {
   if( one > two )
     return two;
@@ -58,25 +43,29 @@ void compare( bool s, bool d, bool u, char* file1, char* file2 ) {
   int read_ret1 = read( fd1, buf1, 1024 );
   int read_ret2 = read( fd2, buf2, 1024 );
 
-  while( expression( s, d, read_ret1, read_ret2 ) ) {
+  while( read_ret1 > 0 && read_ret2 > 0 ) {
     // Convert all to uppercase if ignore case is specified
     if( u )
       upper( buf1, buf2 );
 
-
+    // Actually compare individual characters if the -s option is specified
     if( s ) {
       for( pos = 0; pos < min( read_ret1, read_ret2 ); pos++ ) {
         if( buf1[pos] != buf2[pos] )
           printf("Byte %d    File1:%c	File2:%c\n", acc + pos + 1, buf1[pos], buf2[pos]);
       }
     }
+    // Report whether the files are of different length if the -d flag is set
     if( d ) {
       if( read_ret1 != read_ret2 ) {
         printf("The files are of different length\n");
         exit( 0 );
       }
     }
+    // We're reading 1024 bytes at a time, so we need to keep track of how many
+    // have been read
     acc += min( read_ret1, read_ret2 );
+
     read_ret1 = read( fd1, buf1, 1024 );
     read_ret2 = read( fd2, buf2, 1024 );
   }
--- p2.c
+++ p2.c
@@ -17,6 +17,71 @@ void upper( char *mystring ) {
   }
 }
 
+void process_three( int fd1[], int fd2[] ) {
+  char buf3[1024];
+  close( fd1[1] );
+  close( fd2[0] );
+
+  if( read( fd1[0], buf3, 1024 ) == -1 )
+    fatal_error( "Failed to read from pipe!", 1 );
+  // Done reading from that pipe
+  close( fd1[0] );
+  upper( buf3 );
+
+  write( STDOUT_FILENO, buf3, strlen( buf3 ) );
+  write( STDOUT_FILENO, "\n", 1 );
+
+  write( fd2[1], buf3, strlen( buf3 ) );
+  close( fd2[1] );
+  printf("Terminating process 3\n");
+  exit( 0 );
+}
+
+void process_two( int fd1[], int fd2[] ) {
+  int i, pipe21_fd[2], pipe22_fd[2];
+  char buf2[2048], rev_str[1024], tmp;
+  pid_t child2_pid;
+  if( read( fd1[0], buf2, 1024 ) == -1 )
+    fatal_error( "Read from pipe failed!", 1 );
+  close( fd1[0] );
+  // Reverse the string
+  for( i = strlen( buf2 ) - 1; i >= 0; i-- ) {
+    rev_str[ strlen( buf2 ) - i - 1 ] = buf2[ i ];
+  }
+  write( STDOUT_FILENO, rev_str, strlen( rev_str ) );
+  write( STDOUT_FILENO, "\n", 1 );
+  
+  if( pipe( pipe21_fd ) == -1 )
+    fatal_error( "There was an error creating the pipe!", 1 );
+  if( pipe( pipe22_fd ) == -1 )
+    fatal_error( "There was an error creating the pipe!", 1 );
+
+  if( write( pipe21_fd[1], rev_str, strlen( rev_str ) ) == -1 )
+    fatal_error( "Failed to write to pipe!", 1 );
+  close( pipe21_fd[1] );
+
+  // Create process three
+  child2_pid = fork();
+  if( child2_pid == -1 )
+    fatal_error( "The process failed to fork!", 2 );
+  else if( child2_pid == 0 ) {
+    process_three( pipe21_fd, pipe22_fd );
+  }
+  else {
+    close( pipe21_fd[0] );
+    close( pipe22_fd[1] );
+    char buf4[1024];
+    wait( NULL );
+    read( pipe22_fd[0], buf4, 1024 );
+    close( pipe22_fd[0] );
+    strncat( buf2, buf4, 1024 );
+    write( STDOUT_FILENO, buf2, strlen( buf2 ) );
+    write( STDOUT_FILENO, "\n", 1 );
+    printf( "Terminating process 2\n" );
+    exit( 0 );
+  }
+}
+
 int main( int argc, char *argv[] ) {
   // One pipe for each direction, even though they're technically
   // bidirectional, this is because some things in life suck
@@ -50,63 +115,7 @@ int main( int argc, char *argv[] ) {
   if( child1_pid == -1 )
     fatal_error( "The process failed to fork!", 2 );
   else if( child1_pid == 0 ) {
-    int i, pipe21_fd[2], pipe22_fd[2];
-    char buf2[2048], rev_str[1024], tmp;
-    pid_t child2_pid;
-    if( read( pipe11_fd[0], buf2, 1024 ) == -1 )
-      fatal_error( "Read from pipe failed!", 1 );
-    // Reverse the string
-    for( i = strlen( buf2 ) - 1; i >= 0; i-- ) {
-      rev_str[ strlen( buf2 ) - i - 1 ] = buf2[ i ];
-    }
-    write( STDOUT_FILENO, rev_str, strlen( rev_str ) );
-    write( STDOUT_FILENO, "\n", 1 );
-    
-    if( pipe( pipe21_fd ) == -1 )
-      fatal_error( "There was an error creating the pipe!", 1 );
-    if( pipe( pipe22_fd ) == -1 )
-      fatal_error( "There was an error creating the pipe!", 1 );
-
-    if( write( pipe21_fd[1], rev_str, strlen( rev_str ) ) == -1 )
-      fatal_error( "Failed to write to pipe!", 1 );
-    close( pipe21_fd[1] );
-
-    // Create process three
-    child2_pid = fork();
-    if( child2_pid == -1 )
-      fatal_error( "The process failed to fork!", 2 );
-    else if( child2_pid == 0 ) {
-      char buf3[1024];
-      close( pipe21_fd[1] );
-      close( pipe22_fd[0] );
-
-      if( read( pipe21_fd[0], buf3, 1024 ) == -1 )
-        fatal_error( "Failed to read from pipe!", 1 );
-      // Done reading from that pipe
-      close( pipe21_fd[0] );
-      upper( buf3 );
-
-      write( STDOUT_FILENO, buf3, strlen( buf3 ) );
-      write( STDOUT_FILENO, "\n", 1 );
-
-      write( pipe22_fd[1], buf3, strlen( buf3 ) );
-      close( pipe22_fd[1] );
-      printf("Terminating process 3\n");
-      exit( 0 );
-    }
-    else {
-      close( pipe21_fd[0] );
-      close( pipe22_fd[1] );
-      char buf4[1024];
-      wait( NULL );
-      read( pipe22_fd[0], buf4, 1024 );
-      close( pipe22_fd[0] );
-      strncat( buf2, buf4, 1024 );
-      write( STDOUT_FILENO, buf2, strlen( buf2 ) );
-      write( STDOUT_FILENO, "\n", 1 );
-      printf( "Terminating process 2\n" );
-      exit( 0 );
-    }
+    process_two( pipe11_fd, pipe12_fd );
   }
   else {
     wait( NULL );
@@ -114,4 +123,3 @@ int main( int argc, char *argv[] ) {
   }
   return 0;
 }
-
